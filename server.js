744fdfbcee1b679d67e8686929fcb512533da46d
Xjodia <hoangtungda0110@gmail.com>
2024-02-07 17:34:05 +0700

    second commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => { 
+  res.send('API is working');
+});
+
+app.get('/api', (req, res) => { 
+  res.send('API is working');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
